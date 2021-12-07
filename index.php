f01854e3d4ef841a35e4aa1c08a4bea4f9707b75
ekaterina gisinova <gisinovaekaterina@yandex.ru>
2021-12-07 21:11:18 +0300

    add info about company

--- index.php
+++ index.php
@@ -43,10 +43,25 @@
             <a href="/news" class="nav__link">Новости</a>
             <a href="/feedback" class="nav__link">Отзывы</a>
             <a href="/contact" class="nav__link">Контакты</a>
-        </nav>       
+        </nav> 
+        <div class="company__info">            
+            <a href="#" class="company__logo ">
+                <svg width="115" height="101" viewBox="0 0 115 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M103.808 44V86H99.5477V52.4L83.0477 80.66H80.9477L64.4477 52.58V86H60.1877V44H63.8477L82.0877 75.14L100.148 44H103.808Z" fill="#D9D7D2"/>
+                    <path d="M32.1899 38.26L23.6099 46.96V58H19.1699V16H23.6099V41.38L48.2699 16H53.3699L35.1899 35.02L54.6299 58H49.3499L32.1899 38.26Z" fill="#D9D7D2"/>
+                </svg>
+                <p class="company__logo__title company__item"> Мебель <br>на заказ</p>
+            </a>            
+            <div class="company__contact">
+                <a href="tel: +79219389897" class="company__contact-phone company__item">+7 (921) 938-98-97</a>
+                <a href="mailto: klassik.spb@yandex.ru" class="company__contact-email company__item">klassik.spb@yandex.ru</a>
+            </div>
+            
+        </div>      
     </header>
 
-    <main class="main">                          
+    <main class="main">  
+        <h1><?php the_title() ?>  </h1>                     
     </main>
     
     <footer class="footer">            
